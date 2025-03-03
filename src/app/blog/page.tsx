c900d349db276cc422b4137ba01a78f7cced8d6a
softvoyager <Beautifuldevsworld@gmail.com>
2025-03-02 16:39:23 -0800

    add some comments

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,8 +3,6 @@ import Main from "@/components/layouts/main"
 import Footer from "@/components/layouts/footer"
 
 export default function Page() {
-
-
   return (<>
     <Header uri="blog" />
     <Main uri="blog" />
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,6 +2,9 @@ import Header from "@/components/layouts/header"
 import Main from "@/components/layouts/main"
 import Footer from "@/components/layouts/footer"
 
+/**
+ * component as a Not-found page
+ */
 export default function Page() {
   return (<>
     <Header uri="not-found" />
